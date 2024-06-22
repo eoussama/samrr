8fbd195bc334a244a5cd9929cf928865d6dd1cf5
EOussama <oussama_gtacj@live.fr>
2024-06-22 15:33:39 +0100

    feat: scaffold

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,57 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::u32;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn test(value: &str) -> String {
+    format!("Received value '{}' by Rust!", value)
 }
 
 fn is_steam_installed() -> bool {
+	println!("TODO: Checking Steam is installed...");
     false
 }
 
 fn is_steam_running() -> bool {
+	println!("TODO: Checking an instance of Steam is running...");
     false
 }
 
 fn is_steam_user_connected() -> bool {
+	println!("TODO: Checking a user is connected to Steam...");
     false
 }
 
+fn load_user() -> () {
+	println!("TODO: Loading connected Steam user...")
+}
+
+fn load_games() -> () {
+	println!("TODO: Loading games...");
+
+	load_user();
+	load_game_ids();
+	get_user_games();
+}
+
+fn load_game_ids() -> () {
+	println!("TODO: Loading all game IDs...");
+}
+
+fn get_user_games() -> () {
+	println!("TODO: Filtering out games that are not owned by the user...");
+
+	load_game(10);
+	load_game(480);
+}
+
+fn load_game(id: u32) -> () {
+	println!("TODO: Loading info for game ID {}...", id);
+}
+
 fn main() {
+    println!("Starting Samrr...");
+
     if !is_steam_installed() {
         println!("Error: Could not detected an installed version of Steam.");
         return;
@@ -33,9 +67,10 @@ fn main() {
         return;
     }
 
-    println!("Starting Samrr...");
+	load_games();
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![test])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
+let testInputEl;
+let testMsgEl;
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+async function test() {
+  testMsgEl.textContent = await invoke("test", { value: testInputEl.value });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
+  testInputEl = document.querySelector("#test-input");
+  testMsgEl = document.querySelector("#test-msg");
+
+  document.querySelector("#test-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    greet();
+    test();
   });
 });
