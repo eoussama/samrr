d9a3e841bfb8e048f9d92381acd5b1ae6ee0061b
EOussama <oussama_gtacj@live.fr>
2024-06-22 15:18:54 +0100

    feat: scaffold

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,39 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+fn is_steam_installed() -> bool {
+    false
+}
+
+fn is_steam_running() -> bool {
+    false
+}
+
+fn is_steam_user_connected() -> bool {
+    false
+}
+
 fn main() {
+    if !is_steam_installed() {
+        println!("Error: Could not detected an installed version of Steam.");
+        return;
+    }
+
+    if !is_steam_running() {
+        println!("Error: Could not detect a running instance of Steam.");
+        return;
+    }
+
+    if !is_steam_user_connected() {
+        println!("Error: Could a connected user on Steam.");
+        return;
+    }
+
+    println!("Starting Samrr...");
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
